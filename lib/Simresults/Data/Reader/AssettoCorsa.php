552293870cd906784e5a7b9bc4834fc225898976
Maurice van der Star <mauserrifle@gmail.com>
2013-11-22 21:17:44 +0100

    First work on supporting Assetto Corsa

---
+++ lib/Simresults/Data/Reader/AssettoCorsa.php
@@ -0,0 +1,165 @@
+<?php
+namespace Simresults;
+
+/**
+ * The reader for AssettoCorsa
+ *
+ * TODO: Check session types when more game modes are being released
+ *
+ * @author     Maurice van der Star <mauserrifle@gmail.com>
+ * @copyright  (c) 2013 Maurice van der Star
+ * @license    http://opensource.org/licenses/ISC
+ */
+class Data_Reader_AssettoCorsa extends Data_Reader {
+
+    /**
+     * @see Simresults\Data_Reader::canRead()
+     */
+    public static function canRead($data)
+    {
+       return (bool) json_decode($data, TRUE);
+    }
+
+    /**
+     * @see \Simresults\Data_Reader::getSessions()
+     */
+    public function getSessions()
+    {
+        // Get data
+        $data = json_decode($this->data, TRUE);
+
+        // No session data
+        if ( ! $sessions_data = $this->get($data, 'sessions'))
+        {
+            // Throw exception
+            throw new Exception\Reader('Cannot read the session data');
+        }
+
+        // Init sessions array
+        $sessions = array();
+
+        // Init participants array
+        $participants = array();
+
+        // Get participants
+        $players_data = $this->get($data, 'players', array());
+        foreach ($players_data as $player_data)
+        {
+            // Create driver
+            $driver = new Driver;
+            $driver->setName($this->get($player_data, 'name'));
+
+            // Create participant and add driver
+            $participant = new Participant;
+            $participant->setDrivers(array($driver));
+
+            // Add participant to collection
+            $participants[] = $participant;
+        }
+
+        // Gather all sessions
+        foreach ($sessions_data as $session_data)
+        {
+            // Init session
+            $session = new Session;
+
+            // Practice session by default
+            $type = Session::TYPE_PRACTICE;
+
+            // Check session name to get type
+            // TODO: Should be checked when full game is released. Also create
+            //       tests for it!
+            switch(strtolower($name = $this->get($session_data, 'name')))
+            {
+                case 'qualify session':
+                    $type = Session::TYPE_QUALIFY;
+                    break;
+                case 'warmup session':
+                    $type = Session::TYPE_WARMUP;
+                    break;
+                case 'race session':
+                    $type = Session::TYPE_RACE;
+                    break;
+            }
+
+            // Set session values
+            $session->setType($type)
+                    ->setName($name)
+                    ->setMaxLaps(
+                        (int) $this->get($session_data, 'lapsCount'))
+                    ->setMaxMinutes(
+                        (int) $this->get($session_data, 'duration'));
+
+            // Set game
+            $game = new Game; $game->setName('Assetto Corsa');
+            $session->setGame($game);
+
+            // Set track
+            $track = new Track;
+            $track->setVenue($this->get($data, 'track'));
+            $session->setTrack($track);
+
+            // Set participants
+            $session->setParticipants($participants);
+
+            // Get the laps
+            foreach ($this->get($session_data, 'laps', array()) as $lap_data)
+            {
+                // Init new lap
+                $lap = new Lap;
+
+                // Set participant
+                $lap->setParticipant(
+                    $lap_participant = $participants[$lap_data['car']]);
+
+                // Set first driver of participant as lap driver. AC does
+                // not support swapping
+                $lap->setDriver($lap_participant->getDriver());
+
+                // Set lap number (+1 because AC is zero based)
+                $lap->setNumber($lap_data['lap']+1);
+
+                // Set lap time in seconds
+                $lap->setTime($lap_data['time'] / 1000);
+
+                // Set sector times in seconds
+                foreach ($this->get($lap_data, 'sectors', array())
+                             as $sector_time)
+                {
+                    $lap->addSectorTime($sector_time / 1000);
+                }
+
+                // Add lap to participant
+                $lap_participant->addLap($lap);
+            }
+
+            // Add session to collection
+            $sessions[] = $session;
+        }
+
+        // Return all sessions
+        return $sessions;
+    }
+
+    /**
+     * Retrieve a single key from an array. If the key does not exist in the
+     * array, the default value will be returned instead.
+     *
+     *     // Get the value "username" from $_POST, if it exists
+     *     $username = Arr::get($_POST, 'username');
+     *
+     *     // Get the value "sorting" from $_GET, if it exists
+     *     $sorting = Arr::get($_GET, 'sorting');
+     *
+     * This function is from the Kohana project (http://kohanaframework.org/).
+     *
+     * @param   array   $array      array to extract from
+     * @param   string  $key        key name
+     * @param   mixed   $default    default value
+     * @return  mixed
+     */
+    protected function get($array, $key, $default = NULL)
+    {
+        return isset($array[$key]) ? $array[$key] : $default;
+    }
+}
---
+++ tests/AssettoCorsaReaderTest.php
@@ -0,0 +1,182 @@
+<?php
+use Simresults\Data_Reader_AssettoCorsa;
+use Simresults\Data_Reader;
+use Simresults\Session;
+use Simresults\Participant;
+
+/**
+ * Tests for the Assetto Corsa reader
+ *
+ * TODO: Rebuild using a much bigger and better log when the game offers
+ * this.
+ *
+ * @author     Maurice van der Star <mauserrifle@gmail.com>
+ * @copyright  (c) 2013 Maurice van der Star
+ * @license    http://opensource.org/licenses/ISC
+ */
+class AssettoCorsaReaderTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Set error reporting
+     *
+     * @see PHPUnit_Framework_TestCase::setUp()
+     */
+    protected function setUp()
+    {
+        error_reporting(E_ALL);
+    }
+
+
+    /**
+     * Test exception when no data is supplied
+     *
+     * @expectedException Simresults\Exception\CannotReadData
+     */
+    public function testCreatingNewAssettoCorsaReaderWithInvalidData()
+    {
+        $reader = new Data_Reader_AssettoCorsa('Unknown data for reader');
+    }
+
+
+    /***
+    **** Simple tests that do not fit in the full race log used for testing.
+    **** Most of the below tests are done on modfied XML files
+    ***/
+
+
+    /**
+     * Test exception when the log file has no session included
+     *
+     * @expectedException Simresults\Exception\Reader
+     */
+    public function testCreatingNewAssettoCorsaReaderWithNoSessions()
+    {
+        // The path to the data source
+        $file_path = realpath(__DIR__.'/logs/assettocorsa/nosessions.json');
+
+        // Get the data reader for the given data source
+        $reader = Data_Reader::factory($file_path);
+
+        // Get session
+        $session = $reader->getSession();
+    }
+
+    /**
+     * Test reading the session
+     */
+    public function testReadingSession()
+    {
+        // Get session
+        $session = $this->getWorkingReader()->getSession();
+
+        //-- Validate
+        $this->assertSame(Session::TYPE_PRACTICE, $session->getType());
+        $this->assertSame('Hotlapping Session', $session->getName());
+        $this->assertSame(0, $session->getMaxLaps());
+        $this->assertSame(30, $session->getMaxMinutes());
+        // TODO: Enable this
+        // $this->assertSame(3, $session->getLastedLaps());
+    }
+
+    /**
+     * Test reading the game of a session
+     */
+    public function testReadingSessionGame()
+    {
+        // Get the game
+        $game = $this->getWorkingReader()->getSession()->getGame();
+
+        // Validate game
+        $this->assertSame('Assetto Corsa', $game->getName());
+    }
+
+    /**
+     * Test reading the track of a session
+     */
+    public function testReadingSessionTrack()
+    {
+        // Get the track
+        $track = $this->getWorkingReader()->getSession()->getTrack();
+
+        // Validate track
+        $this->assertSame('imola', $track->getVenue());
+    }
+
+    /**
+     * Test reading the participants of a session
+     */
+    public function testReadingSessionParticipants()
+    {
+        // Get participant
+        $participants = $this->getWorkingReader()->getSession()
+            ->getParticipants();
+        $participant = $participants[0];
+
+        $this->assertSame('Maurice van der Star',
+                          $participant->getDriver()->getName());
+    }
+
+    /**
+     * Test reading laps of participants
+     */
+    public function testReadingLapsOfParticipants()
+    {
+        // Get participants
+        $participants = $this->getWorkingReader()->getSession()
+            ->getParticipants();
+
+        // Get the laps of first participants
+        $laps = $participants[0]->getLaps();
+
+        // Validate we have 7 laps
+        $this->assertSame(7, count($laps));
+
+        // Get driver of first participant (only one cause there are no swaps)
+        $driver = $participants[0]->getDriver();
+
+        // Get first lap only
+        $lap = $laps[0];
+
+        // Validate lap
+        $this->assertSame(1, $lap->getNumber());
+        $this->assertSame(119.404, $lap->getTime());
+        $this->assertSame($participants[0], $lap->getParticipant());
+        $this->assertSame($driver, $lap->getDriver());
+
+        // Get sector times
+        $sectors = $lap->getSectorTimes();
+
+        // Validate sectors
+        $this->assertSame(34.668, $sectors[0]);
+        $this->assertSame(46, $sectors[1]);
+        $this->assertSame(38.736, $sectors[2]);
+    }
+
+
+    // Code...
+    // 119404 = 119.404
+
+
+    /**
+     * Get a working reader
+     */
+    protected function getWorkingReader()
+    {
+        static $reader;
+
+        // Reader aready created
+        if ($reader)
+        {
+            return $reader;
+        }
+
+        // The path to the data source
+        $file_path = realpath(__DIR__.'/logs/assettocorsa/offline_hotlap_session.json');
+
+        // Get the data reader for the given data source
+        $reader = Data_Reader::factory($file_path);
+
+        // Return reader
+        return $reader;
+    }
+}
