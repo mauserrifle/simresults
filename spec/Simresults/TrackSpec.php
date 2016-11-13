71bb4d5fd7bb6723292236e795cafeebd3582308
Maurice van der Star <mauserrifle@gmail.com>
2016-11-13 12:54:05 +0100

    Track spec replaces old phpunit tests

---
+++ spec/Simresults/TrackSpec.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace spec\Simresults;
+
+use Simresults\Track;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class TrackSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Track::class);
+    }
+
+    function it_returns_friendly_name_using_venue_course_event()
+    {
+        // Set names
+        $this->setVenue('Sebring [Virtua_LM]')
+             ->setCourse('Sebring 12h Course ( /1 # `') // Set (, /, #, ` chars
+             ->setEvent('12h Course');                  // to test for regex
+                                                        // errors that
+                                                        // previously occured
+        $this->getFriendlyName()->shouldReturn(
+        	'Sebring [Virtua_LM], Sebring 12h Course ( /1 # `');
+
+        // Change event name
+        $this->setEvent('12h Alternative course');
+
+        $this->getFriendlyName() ->shouldReturn(
+        	'Sebring [Virtua_LM], Sebring 12h Course ( /1 # ` '.
+        	'(12h Alternative course)');
+    }
+}
