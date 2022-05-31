6715721bcc971e201aff67c48e1a47a84b64f112
Maurice van der Star <mauserrifle@gmail.com>
2022-05-31 08:30:51 +0200

    ProjectCarsServer: Quickfixes to prevent some errors on empty logs

--- lib/Simresults/Data/Reader/ProjectCarsServer.php
+++ lib/Simresults/Data/Reader/ProjectCarsServer.php
@@ -292,6 +292,7 @@ class Data_Reader_ProjectCarsServer extends Data_Reader {
                 $participants_with_events = array();
 
                 // Parse events first only to collect missing participants
+                if (isset($session_data['events']))
                 foreach ($session_data['events'] as $event)
                 {
                     // Participant unknown
@@ -308,6 +309,7 @@ class Data_Reader_ProjectCarsServer extends Data_Reader {
                 $cut_data = array();
                 $driver_has_entered_pit = array();
                 $finished_participants_by_id = array();
+                if (isset($session_data['events']))
                 foreach ($session_data['events'] as $event)
                 {
                     // Get participant
@@ -590,6 +592,7 @@ class Data_Reader_ProjectCarsServer extends Data_Reader {
                  */
 
                 // Get additional info from participants entries
+                if (isset($history['participants']))
                 foreach ($history['participants'] as $part_data)
                 {
                     // Driver not known
